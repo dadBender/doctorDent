76cc1a27a6f50b019e83fd6dd1a832fefb1d14f5
dadBenders <ntnefimov@gmail.com>
2024-03-30 13:31:28 +0300

    фищи

--- scripts/script.js
+++ scripts/script.js
@@ -36,7 +36,49 @@ function updateSlider() {
 // Инициализация слайдера
 updateSlider();
 
+if (document.body.clientWidth < 1100){
+  console.log("bugaga");
+const SamplesSlider = document.querySelector(".samples__container");
+console.log('.samples__container');
+const SamplesPrevButton = document.querySelector(".works__prev-button");
+const SamplesNextButton = document.querySelector(".works__next-button");
+const SamplesSlides = Array.from(SamplesSlider.querySelectorAll(".samples__card__container"));
+const SamplesSlideCount = SamplesSlides.length;
+let SamplesSlideIndex = 0;
 
+// Устанавливаем обработчики событий для кнопок
+SamplesPrevButton.addEventListener("click", SampleshowPreviousSlide);
+SamplesNextButton.addEventListener("click", SampleshowNextSlide);
+
+// Функция для показа предыдущего слайда
+function SampleshowPreviousSlide() {
+  console.log('prev');
+  SamplesSlideIndex = (SamplesSlideIndex - 1 + SamplesSlideCount) % SamplesSlideCount;
+  SampleupdateSlider();
+}
+
+// Функция для показа следующего слайда
+function SampleshowNextSlide() {
+  console.log('next');
+  SamplesSlideIndex = (SamplesSlideIndex + 1) % SamplesSlideCount;
+  SampleupdateSlider();
+}
+
+// Функция для обновления отображения слайдера
+function SampleupdateSlider() {
+  SamplesSlides.forEach((slide, index) => {
+    if (index === SamplesSlideIndex) {
+      slide.style.display = "block";
+    } else {
+      slide.style.display = "none";
+    }
+  });
+}
+
+// Инициализация слайдера
+SampleupdateSlider();
+
+}
 
 
 
