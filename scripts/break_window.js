7f65f02f1ff2a178551cdbd0ae96883af90b1835
Anton Efimov <ntnefimov@gmail.com>
2024-04-15 07:35:56 +0300

    aaa

--- scripts/break_window.js
+++ scripts/break_window.js
@@ -1,54 +1,54 @@
 function BreakWindow() {
-    const SamplesSlider = document.querySelector(".samples__container");
-    console.log(".samples__container");
-    const SamplesPrevButton = document.querySelector(".works__prev-button");
-    const SamplesNextButton = document.querySelector(".works__next-button");
-    const SamplesSlides = Array.from(
-      SamplesSlider.querySelectorAll(".samples__card__container")
-    );
-    const SamplesSlideCount = SamplesSlides.length;
-    let SamplesSlideIndex = 0;
-  
-    // Устанавливаем обработчики событий для кнопок
-    SamplesPrevButton.addEventListener("click", SampleshowPreviousSlide);
-    SamplesNextButton.addEventListener("click", SampleshowNextSlide);
-  
-    // Функция для показа предыдущего слайда
-    function SampleshowPreviousSlide() {
-      console.log("prev");
-      SamplesSlideIndex = (SamplesSlideIndex - 1 + SamplesSlideCount) % SamplesSlideCount;
-      SampleupdateSlider();
-    }
-  
-    // Функция для показа следующего слайда
-    function SampleshowNextSlide() {
-      console.log("next");
-      SamplesSlideIndex = (SamplesSlideIndex + 1) % SamplesSlideCount;
-      SampleupdateSlider();
-    }
-  
-    // Функция для обновления отображения слайдера
-    function SampleupdateSlider() {
-      SamplesSlides.forEach((slide, index) => {
-        if (index === SamplesSlideIndex) {
-          slide.style.display = "block";
-        } else {
-          slide.style.display = "none";
-        }
-      });
-    }
-  
-    // Инициализация слайдера
+  const SamplesSlider = document.querySelector(".samples__container");
+  console.log(".samples__container");
+  const SamplesPrevButton = document.querySelector(".works__prev-button");
+  const SamplesNextButton = document.querySelector(".works__next-button");
+  const SamplesSlides = Array.from(
+    SamplesSlider.querySelectorAll(".samples__card__container")
+  );
+  const SamplesSlideCount = SamplesSlides.length;
+  let SamplesSlideIndex = 0;
+
+  // Устанавливаем обработчики событий для кнопок
+  SamplesPrevButton.addEventListener("click", SampleshowPreviousSlide);
+  SamplesNextButton.addEventListener("click", SampleshowNextSlide);
+
+  // Функция для показа предыдущего слайда
+  function SampleshowPreviousSlide() {
+    console.log("prev");
+    SamplesSlideIndex = (SamplesSlideIndex - 1 + SamplesSlideCount) % SamplesSlideCount;
     SampleupdateSlider();
   }
-  
+
+  // Функция для показа следующего слайда
+  function SampleshowNextSlide() {
+    console.log("next");
+    SamplesSlideIndex = (SamplesSlideIndex + 1) % SamplesSlideCount;
+    SampleupdateSlider();
+  }
+
+  // Функция для обновления отображения слайдера
+  function SampleupdateSlider() {
+    SamplesSlides.forEach((slide, index) => {
+      if (index === SamplesSlideIndex) {
+        slide.style.display = "block";
+      } else {
+        slide.style.display = "none";
+      }
+    });
+  }
+
   // Инициализация слайдера
-  updateSlider();
+  SampleupdateSlider();
+}
+
+// Инициализация слайдера
+updateSlider();
 //   if (window.matchMedia("(max-width: 1100px)").matches) {
 //     BreakWindow();
 //     console.log("bugaga");
 //   }
-  
+
 // const mobileWidthMediaQuery = window.matchMedia('(max-width: 1100px)')
 
 // function printLog(isMobileSize) {
--- scripts/swaper_qualif.js
+++ scripts/swaper_qualif.js
@@ -1,92 +1,126 @@
 function DoctorsInfo () {
-let Doctors = document.querySelectorAll('.doctors__card');
-let ShownDoctor;
-    
-for (let i = 0; i < Doctors.length; i++) {
-    if (Doctors[i].classList.contains('visually-hidden')) {
-    }
-    else {
-        ShownDoctor = Doctors[i]; 
-    }
-}
-let doctorsEducationText = ShownDoctor.querySelectorAll(".doctors__card__text-item");
-
-
-
-let DoctorsQualificationButtons = ShownDoctor.querySelectorAll('.doctors__button');
-
-let DoctorsQualificationShown;
-let ShownText;
-
-function NotUseButton() {
-    for (let i = 0; i < DoctorsQualificationButtons.length; i++) {
-        DoctorsQualificationButtons[i].classList.add("doctors__button-none");
-    }
-}
-
-for (let i = 0; i < DoctorsQualificationButtons.length; i++) {
-    DoctorsQualificationButtons[i].addEventListener("click", function (event) {
-       
-
-        if (event.target.classList.contains("doctors__button__education")) {
-            NotUseButton();
-            event.target.classList.remove("doctors__button-none");
-
-            for (let i = 0; i < doctorsEducationText.length; i++){
-               if (doctorsEducationText[i].classList.contains("education__text") ) {
-                doctorsEducationText[i].classList.remove("visually-hidden");
-               }
-               else {
-                doctorsEducationText[i].classList.add("visually-hidden")
-               }
-            }
-        }
-        if (event.target.classList.contains("doctors__button__specification")) {
-            NotUseButton();
-            event.target.classList.remove("doctors__button-none");
-
-            for (let i = 0; i < doctorsEducationText.length; i++){
-                if (doctorsEducationText[i].classList.contains("specification__text") ) {
-                doctorsEducationText[i].classList.remove("visually-hidden");
-                 
-                }
-                else {
-                 doctorsEducationText[i].classList.add("visually-hidden")
-                }
-             }
-        }
-        if (event.target.classList.contains("doctors__button__seminars")) {
-            NotUseButton();
-            event.target.classList.remove("doctors__button-none");
-            for (let i = 0; i < doctorsEducationText.length; i++){
-                if (doctorsEducationText[i].classList.contains("seminars__text") ) {
-                doctorsEducationText[i].classList.remove("visually-hidden");
-                 
-                }
-                else {
-                 doctorsEducationText[i].classList.add("visually-hidden")
-                }
-             }
-        }
+    let Doctors = document.querySelectorAll('.doctors__card');
+    let ShownDoctor;
         
-        
-    })
-}
-
-}
-DoctorsInfo();
-
-function DoctorsInfoStart() {
-    let StartText = document.querySelectorAll(".doctors__card__text-item");
-    for (let i = 0; i < StartText.length; i++){
-        if (StartText[i].classList.contains("education__text") ) {
-            StartText[i].classList.remove("visually-hidden");
-         
+    for (let i = 0; i < Doctors.length; i++) {
+        if (Doctors[i].classList.contains('visually-hidden')) {
         }
         else {
-            StartText[i].classList.add("visually-hidden")
+            ShownDoctor = Doctors[i]; 
         }
-     }
-}
-
-DoctorsInfoStart();
+    }
+    let doctorsEducationText = ShownDoctor.querySelectorAll(".doctors__card__text-item");
+    
+    
+    
+    let DoctorsQualificationButtons = ShownDoctor.querySelectorAll('.doctors__button');
+    
+    let DoctorsQualificationShown;
+    let ShownText;
+    
+    function NotUseButton() {
+        for (let i = 0; i < DoctorsQualificationButtons.length; i++) {
+            DoctorsQualificationButtons[i].classList.add("doctors__button-none");
+        }
+    }
+    
+    for (let i = 0; i < DoctorsQualificationButtons.length; i++) {
+        DoctorsQualificationButtons[i].addEventListener("click", function (event) {
+           
+    
+            if (event.target.classList.contains("doctors__button__education")) {
+                NotUseButton();
+                event.target.classList.remove("doctors__button-none");
+    
+                for (let i = 0; i < doctorsEducationText.length; i++){
+                   if (doctorsEducationText[i].classList.contains("education__text") ) {
+                    doctorsEducationText[i].classList.remove("visually-hidden");
+                   }
+                   else {
+                    doctorsEducationText[i].classList.add("visually-hidden")
+                   }
+                }
+            }
+            if (event.target.classList.contains("doctors__button__specification")) {
+                NotUseButton();
+                event.target.classList.remove("doctors__button-none");
+    
+                for (let i = 0; i < doctorsEducationText.length; i++){
+                    if (doctorsEducationText[i].classList.contains("specification__text") ) {
+                    doctorsEducationText[i].classList.remove("visually-hidden");
+                     
+                    }
+                    else {
+                     doctorsEducationText[i].classList.add("visually-hidden")
+                    }
+                 }
+            }
+            if (event.target.classList.contains("doctors__button__seminars")) {
+                NotUseButton();
+                event.target.classList.remove("doctors__button-none");
+                for (let i = 0; i < doctorsEducationText.length; i++){
+                    if (doctorsEducationText[i].classList.contains("seminars__text") ) {
+                    doctorsEducationText[i].classList.remove("visually-hidden");
+                     
+                    }
+                    else {
+                     doctorsEducationText[i].classList.add("visually-hidden")
+                    }
+                 }
+            }
+            
+            
+        })
+    }
+    
+    }
+    DoctorsInfo();
+    
+    function DoctorsInfoStart() {
+        let StartText = document.querySelectorAll(".doctors__card__text-item");
+        for (let i = 0; i < StartText.length; i++){
+            if (StartText[i].classList.contains("education__text") ) {
+                StartText[i].classList.remove("visually-hidden");
+             
+            }
+            else {
+                StartText[i].classList.add("visually-hidden")
+            }
+         }
+    }
+    
+    DoctorsInfoStart();
+    
+    
+    const swiper__office = new Swiper(".swiper__office", {
+        autoplay: {
+            delay: 5000,
+          },
+       slidesPerView: 4,
+       spaceBetween: 8,
+       loop: true,
+       centeredSlides: true,
+       breakpoints: {
+        300: {
+            slidesPerView: 1.8,
+        },
+        500: {
+            slidesPerView: 2.3,
+        },
+        700: {
+            slidesPerView: 2.8,
+        },
+        1000: {
+            slidesPerView: 3.2,
+    
+        },
+        1200: {
+            slidesPerView: 3.5,
+    
+        },
+        1500: {
+            slidesPerView: 4,
+        },
+      },
+      });
+      
